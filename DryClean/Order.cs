6501406ae5d4adc605e706eb9aa08fb73d36f7f7
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-09-20 00:43:31 +0300

    Add files via upload

---
+++ DryClean/Order.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryClean
+{
+    public class Order
+    {
+        public int OrderId {  get; set; }
+
+        public string Status {  get; set; }
+
+        private List<Item> Items { get; set; }
+
+        private List<Service> services { get; set; }
+
+        private Payment payment { get; set; } // Dynamic
+
+        public Order()
+        {
+            Items = new List<Item>();
+            services = new List<Service>();
+            Console.WriteLine($"Order is Created {DateTime.Now}...");
+        }
+
+
+        public double CalculateTotal()
+        {
+            double total = 0;
+
+            foreach (var item in Items)
+            {
+                total += item.GetPrice();
+            }
+            foreach (var service in services)
+            {
+                total += service.GetPrice();
+            }
+            return total;
+        }
+        
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void AddService(Service service)
+        {
+            services.Add(service);
+        }
+
+        public void AddPayment(Payment pay) // Dynamic 
+        {
+            payment = pay;
+            payment.ProcessPaymentMessage();
+        }
+
+
+        public void FinishOrder()
+        {
+            payment = null;
+            Console.WriteLine("Thanks for using our services..");
+        }
+    }
+}
---
+++ DryClean/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DryClean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer()
+            {
+                Name = "Gaber mohamed",
+                Phone = "01002387286",
+                Email = "GM@gmail.com",
+                Address = "Alexandria"
+            };
+
+
+
+            Order o1 = new Order()
+            {
+                OrderId = 1,
+                Status = "Created"
+            };
+            c1.MakeOrder(o1);
+            
+            Item i1 = new Item( 1 , "TShirt" , 350);
+            Item i2 = new Item(2, "Suit", 400);
+
+           c1.order.AddItem(i1);
+            c1.order.AddItem(i2);
+
+            Service s1 = new Service(1, "Washing", 50);
+            Service s2 = new Service(2, "Washing", 40);
+
+            c1.order.AddService(s1);
+            c1.order.AddService(s2);
+
+            double total = o1.CalculateTotal();
+            Console.WriteLine($"Total Salary = {total}");
+
+
+            CreditCardPayment p1 = new CreditCardPayment()
+            {
+                Amount = total
+            };
+            p1.SetNumber(5741);
+            
+            c1.order.AddPayment(p1);
+           
+
+            // Make Bill
+            Bill b1 = new Bill(o1)
+            {
+                BillId = 45
+            };
+            Console.WriteLine(b1.PrintBill(p1));
+
+            c1.order.FinishOrder(); // Close App
+
+        }
+    }
+}
